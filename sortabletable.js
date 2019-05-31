420c95e6fc51854ac038dedd363475455b240b16
Master Shivang Joshi <shivang.g.joshi@gmail.com>
2019-05-31 18:38:05 +0530

    Arrangable fields! final commit on 1st resume

--- sortabletable.js
+++ sortabletable.js
@@ -1,5 +1,53 @@
 
-$('table tbody').sortable({
-    animation: 1500,
-    ghostClass: 'tag',
-});
+function EnableDragArrange(){
+
+    $('table tbody').sortable({
+        animate:150,
+        placeholder: 'rowbackground',    /*name of the CSS class*/
+    });
+    
+}
+
+function DestroyDragArrange(){
+    $('table tbody').sortable("destroy");
+}
+
+function arrange(element) {
+
+    var tablerows = element.parentNode  //td
+    .parentNode //tr
+    .parentNode //tfoot
+    .parentNode //table
+    .tBodies[0].rows;
+    
+    if(element.innerHTML == "Arrange"){       
+        for(var i=0;i<tablerows.length;i++)
+        {
+            tablerows[i].cells[0].style.paddingLeft = '10px';
+            tablerows[i].style.background = 'rgba(28, 206, 230, 0.753)';
+            tablerows[i].style.borderTop = '1px solid aliceblue';
+            /*
+            this effect is given to rows when arrange button is clicked
+            */
+        }
+        element.innerText = 'Edit';
+        EnableDragArrange();
+    }
+
+    else if(element.innerText == 'Edit'){
+        for(var i=0;i<tablerows.length;i++)
+        {
+            tablerows[i].cells[0].style.paddingLeft = '0px';
+            tablerows[i].style.background = 'inherit';
+            tablerows[i].style.borderTop = 'none';
+            /*
+            this effect is given to rows when Edit button is clicked
+            */
+        }
+        element.innerText = 'Arrange';    
+        DestroyDragArrange();
+    }
+
+}
+
+
